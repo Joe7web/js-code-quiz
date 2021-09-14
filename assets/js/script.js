166da3bc0c028a974daa8438d662c62469f270b2
JDGK <jsweb44@gmail.com>
2021-09-14 18:36:11 -0400

    updated questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,46 @@
+var firstPage =  document.querySelector(".first-page");
+var btnStartQuiz =  document.querySelector(".start-button");
+var timerCount = document.querySelector(".timer-count");
 
 
+ //Make an array of objects?! Put all questions and answers in objects
+ //Not sure which way
+var questionsAnswers = {
+questOne:"Commonly used data types DO NOT include:",
+choicesOne:['1. strings', '2. booleans', '3. alerts ','4. numbers'],
+correctAnsOne:['3.alerts'],
 
+questTwo:"The condition in an if / else statement is enclosed within ______",
+choicesTwo:['1. quotes','2. curly brackets', '3. parentheses','4. square brackets'],
+correctAnsTwo:['3. parentheses'],
 
+questThree:"Arrays in JavaScript can be used to store ______ when being assigned to variables",
+choicesThree:['1. numbers and strings','2. other arrays','3. booleans','4. all of the above' ],
+correctAnsThree:['4. all of the above'],
+
+questFour:"String values must be enclosed within_____ when being assigned to variables.",
+choicesFour:['1. commas','2. curly brackets', '3. quotes', '4. paretheses'],
+correctAnsFour:['3. quotes'],
+
+questFive:"A very useful tool used during development and debugging for printing content to the debugger is",
+choicesFive:['1.JavaScript','2. terminal/bash', '3. for loops', '4. console.log'],
+correctAnsFive:['4.console.log'],
+ 
+}
+
+// more something like this ?????? In an array??
+
+// {questions:"1. text"
+// choices:[1 ex,2 ex,3 ex,4 ex]
+// anwer: "correct answer"}
+
+// {questions:"2. text"
+// choices:[ ex, ex, ex, ex]
+// anwer: "correct answer"}
+
+// {questions:"3. text"
+// choices:[1 ex,2 ex,3 ex,4 ex]
+// anwer: "correct answer"}
 
 
 
