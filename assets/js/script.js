0a12fc66e9fa98715b76d0c2f68cf499f3d01b48
JDGK <jsweb44@gmail.com>
2021-09-15 22:01:13 -0400

    set up question switcher function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var firstPage =  document.querySelector(".first-page");
+
 var startButton =  document.querySelector(".start-button");
 var timerCount = document.querySelector(".timer-count");
 var timer = 25;
@@ -11,6 +11,11 @@ var thirdQuestion = document.querySelector(".third-question");
 var fourthQuestion = document.querySelector(".fourth-question");
 var fifthQuestion = document.querySelector(".fifth-question");
 
+var firstChoices = document.querySelector("#choices-one");
+var secondChoices = document.querySelector("#choices-two");
+var thirdChoices = document.querySelector("#choices-three");
+var fourthChoices = document.querySelector("#choices-four");
+var fifthChoices = document.querySelector("#choices-five");
 
 
 function startQuiz(event) {
@@ -27,16 +32,22 @@ function startQuiz(event) {
 
   }, 1000)
 
- 
+ firstPage.style.display='none';
+questionSwitcher()
 }
 
 startButton.addEventListener('click', startQuiz)
 
-startButton.addEventListener('click', questionSwitcher)
+//startButton.addEventListener('click', questionSwitcher)
 
-function questionSwitcher(event) {
-  questOne.question 
-    console.log(questOne.question);
+function questionSwitcher() {
+  firstQuestion.textContent = questionsArray[0].question
+  firstChoices.textContent = questionsArray[0].choices[0]
+  secondChoices.textContent = questionsArray[0].choices[1]
+  thirdChoices.textContent = questionsArray[0].choices[2]
+  fourthChoices.textContent = questionsArray[0].choices[3]
+
+  console.log(questionsArray);
 }
 
 
@@ -44,36 +55,50 @@ function questionSwitcher(event) {
 
 //var questions = ['first', 'quest', 'second' ]
 //var arrayOfQuests = []
+var questionsArray = [ 
+  {
+  question:"Commonly used data types DO NOT include:",
+  choices:['1. strings', '2. booleans', '3. alerts','4. numbers'],
+  correctAns:['3. alerts']
+},
+{
+  question:"Commonly used data types DO NOT include:",
+  choices:['1. strings', '2. booleans', '3. alerts','4. numbers'],
+  correctAns:['3. alerts']
+},
+{
+  question:"Commonly used data types DO NOT include:",
+  choices:['1. strings', '2. booleans', '3. alerts','4. numbers'],
+  correctAns:['3. alerts'],
+  }
+]
 
-var questOne = {
-question:"Commonly used data types DO NOT include:",
-choices:['1. strings', '2. booleans', '3. alerts ','4. numbers'],
-correctAns:['3.alerts'],
-}
 
-var questTwo = {
-question:"The condition in an if / else statement is enclosed within ______",
-choices:['1. quotes','2. curly brackets', '3. parentheses','4. square brackets'],
-correctAns:['3. parentheses'],
-}
 
-var questThree = {
-question:"Arrays in JavaScript can be used to store ______ when being assigned to variables",
-choices:['1. numbers and strings','2. other arrays','3. booleans','4. all of the above' ],
-correctAns:['4. all of the above'],
-}
+//}
 
-var questFour = {
-question:"String values must be enclosed within_____ when being assigned to variables.",
-choices:['1. commas','2. curly brackets', '3. quotes', '4. paretheses'],
-correctAns:['3. quotes'],
-}
+//  = {
+// question:"The condition in an if / else statement is enclosed within ______",
+// choices:['1. quotes','2. curly brackets', '3. parentheses','4. square brackets'],
+// correctAns:['3. parentheses'],
+// }
 
-var questFive = {
-question:"A very useful tool used during development and debugging for printing content to the debugger is",
-choices:['1.JavaScript','2. terminal/bash', '3. for loops', '4. console.log'],
-correctAns:['4.console.log'],
-} 
+//  = {
+// question:"Arrays in JavaScript can be used to store ______ when being assigned to variables",
+// choices:['1. numbers and strings','2. other arrays','3. booleans','4. all of the above' ],
+// correctAns:['4. all of the above'],
+// }
+
+//  = {
+// question:"String values must be enclosed within_____ when being assigned to variables.",
+// choices:['1. commas','2. curly brackets', '3. quotes', '4. paretheses'],
+// correctAns:['3. quotes'],
+// }
+//  = {
+// question:"A very useful tool used during development and debugging for printing content to the debugger is",
+// choices:['1.JavaScript','2. terminal/bash', '3. for loops', '4. console.log'],
+// correctAns:['4.console.log'],
+// } 
 
 
 
